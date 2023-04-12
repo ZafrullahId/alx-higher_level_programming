ae3f67ef76b7b91348040ae8f76be2a653db8c8d
ZafrullahId <zafrullahdaprince@gmail.com>
2023-04-12 05:47:22 -0700

    0-print_list_integer.py file added

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# printing from a list
+# Zafrullah Idris
+
+def print_list_integer(my_list):      
+    for n in my_list:
+        print("{}".format(n))
+
+
+my_list = [1, 2, 3, 4, 5]
+print_list_integer(my_list)
