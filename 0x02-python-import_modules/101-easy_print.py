17aa6f4199cb24cfeb411349229a0db1f7a4170a
ZafrullahId <zafrullahdaprince@gmail.com>
2023-04-12 05:25:20 -0700

    101-easy_print.py file added

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,8 +1,2 @@
 #!/usr/bin/python3
-# 101-easy_print.py
-# Zafrullah Idris
-
-if __name__ == "__main__":
-    """Print #pythoniscool without using print or eval or open or sys"""
-
-    __import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
+__import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
