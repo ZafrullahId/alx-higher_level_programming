60038b8961c01e5c55cad999dea9180303a67e14
root <ZafrullahId>
2023-05-22 08:48:15 -0700

    2-replace_in_list.py file added

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,9 +2,7 @@
 # printing from a list
 # Zafrullah Idris
 
-def replace_in_list(my_list, idx, element):
-    """replaces an element of a list at a specific"""
-    if idx < 0 or idx > (len(my_list) - 1):
-        return my_list
-    my_list[idx] = element
-    return my_list
+print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
+
+my_list = [1, 2, 3]
+print_reversed_list_integer(my_list)
